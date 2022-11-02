48e2c5482eaed37e0c5a19dac1d5e6b5279508e4
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-02 01:22:07 -0300

    test: add create manager valid body and saved mock

---
+++ tests/managerMock.ts
@@ -0,0 +1,13 @@
+export const createManagerMock = {
+  validBody: {
+    name: 'any-valid-name',
+    email: 'any-valid-email',
+    password: 'any-valid-password'
+  },
+
+  savedManager: {
+    id: 'any-valid-id',
+    name: 'any-valid-name',
+    email: 'any-valid-email',
+  }
+}
