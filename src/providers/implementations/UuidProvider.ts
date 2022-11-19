1841dcea7704182d2826a27da083c9932e2b86f1
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-19 19:30:48 -0300

    fix: id provider import

--- src/providers/implementations/UuidProvider.ts
+++ src/providers/implementations/UuidProvider.ts
@@ -1,4 +1,4 @@
-import uuid from 'uuid';
+import * as uuid from 'uuid';
 import { IIdGeneratorProvider } from '../IIdProvider';
 
 export default class UuidProvider implements IIdGeneratorProvider {
