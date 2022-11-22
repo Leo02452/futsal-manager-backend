f887c185817c2b58d56e7cf9c0ce81c49da46cee
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-22 15:09:22 -0300

    feat: add custom error message on validation of create manager body

--- src/providers/implementations/zodValidator/schemas/CreateManager.ts
+++ src/providers/implementations/zodValidator/schemas/CreateManager.ts
@@ -1,9 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { z } from 'zod';
 
 const createManagerSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
+  name: z.string({
+    required_error: 'Missing params: name',
+    invalid_type_error: 'name must be a string',
+  }),
+  email: z.string({
+    required_error: 'Missing params: email',
+    invalid_type_error: 'email must be a string',
+  }).email(),
+  password: z.string({
+    required_error: 'Missing params: password',
+    invalid_type_error: 'password must be a string',
+  }),
 });
 
 type createManagerDTO = z.infer<typeof createManagerSchema>;
