9b241e042c6109cbda48f860fd14533902041d04
Leo Araujo <leonardo_02452@hotmail.com>
2022-12-12 12:44:00 -0300

    refactor: change routes import/export

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
-import managerRoutes from './routes/ManagerRoutes';
-import authRoutes from './routes/AuthRoutes';
-import playerRoutes from './routes/PlayerRoutes';
+import {
+  authRoutes,
+  managerRoutes,
+  playerRoutes,
+} from './routes';
 import errorHandler from './middlewares/errorHandler';
 
 class App {
