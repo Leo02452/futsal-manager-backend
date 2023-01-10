9433f191284ad2fe61f5ce824657d3db19bf02cb
Leo Araujo <leonardo_02452@hotmail.com>
2023-01-10 19:40:46 -0300

    feat: add not found error class and middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -4,11 +4,13 @@ import {
   ConflictError,
   UnauthorizedError,
   UnprocessableEntityError,
+  NotFoundError,
 } from '../helpers/errors';
 
 const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof ZodError) return res.status(400).json({ error: err.issues[0].message });
-  if (err instanceof UnauthorizedError) return res.status(404).json({ error: err.message });
+  if (err instanceof UnauthorizedError) return res.status(401).json({ error: err.message });
+  if (err instanceof NotFoundError) return res.status(404).json({ error: err.message });
   if (err instanceof ConflictError) return res.status(409).json({ error: err.message });
   if (err instanceof UnprocessableEntityError) return res.status(422).json({ error: err.message });
   return res.status(500).json({ error: err.message });
