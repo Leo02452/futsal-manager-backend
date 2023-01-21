030f4edc1e5e0cd9b4e3b0eba14e5c0db6376674
Leo Araujo <leonardo_02452@hotmail.com>
2023-01-21 12:12:24 -0300

    feat: implements list matches route

--- src/routes/MatchRoutes.ts
+++ src/routes/MatchRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import CreateMatchControllerFactory
   from '../factories/implementations/CreateMatchControllerFactory';
+import ListMatchesControllerFactory
+  from '../factories/implementations/ListMatchesControllerFactory';
 
 const createMatchController = CreateMatchControllerFactory.make();
+const listMatchesController = ListMatchesControllerFactory.make();
 
 const route = Router();
 
@@ -10,6 +13,14 @@ route.post(
   '/',
   async (req, res) => {
     const response = await createMatchController.handle(req);
+    return res.status(response.status).json(response.body);
+  },
+);
+
+route.get(
+  '/',
+  async (_req, res) => {
+    const response = await listMatchesController.handle();
     return res.status(response.status).json(response.body);
   },
 );
