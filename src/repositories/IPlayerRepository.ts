be72257890b653ccffdd18882a66d47f7c6e467a
Leo Araujo <leonardo_02452@hotmail.com>
2023-01-23 20:06:29 -0300

    feat: add list players repository interface and implementation

--- src/repositories/IPlayerRepository.ts
+++ src/repositories/IPlayerRepository.ts
@@ -4,3 +4,7 @@ export interface ICreatePlayerRepository {
   findByName(name: string, teamId: string): Promise<ICreatedPlayer | null>
   save(player: IPlayer): Promise<void>
 }
+
+export interface IListPlayersRepository {
+  getAll(teamId: string): Promise<IPlayer[]>
+}
