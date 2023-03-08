21f76fdfd2a70f9cb3b714666bc135de35c030a4
Leo Araujo <leonardo_02452@hotmail.com>
2023-03-08 12:45:10 -0300

    test: ensure CreateMatchPlayerService returns nothing

---
+++ tests/unit/createMatchPlayerService.test.ts
@@ -0,0 +1,33 @@
+import { expect, use } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import sinon from "sinon";
+import prismaModel from "../../src/database/prisma";
+import MatchPlayerRepository from "../../src/repositories/implementations/MatchPlayerRepository";
+import PlayerRepository from "../../src/repositories/implementations/PlayerRepository";
+import CreateMatchPlayerService from "../../src/services/CreateMatchPlayerService";
+import { createMatchPlayerMock } from "../mocks/matchPlayerMock";
+
+use(chaiAsPromised);
+
+describe('Create Match Player Service', () => {
+  const matchPlayerRepository = new MatchPlayerRepository(prismaModel.matchPlayer);
+  const playerRepository = new PlayerRepository(prismaModel.player);
+  const createMatchPlayerService = new CreateMatchPlayerService(
+    matchPlayerRepository,
+    playerRepository,
+  );
+
+  afterEach(sinon.restore);
+  describe('On Success', () => {
+    it('should return nothing', () => {
+      sinon.stub(playerRepository, 'findByParam').resolves();
+      sinon.stub(matchPlayerRepository, 'save').resolves();
+
+      return expect(
+        createMatchPlayerService.execute(createMatchPlayerMock.validDTO)
+      ).to.eventually.be.equals(undefined);
+    });
+
+  });
+
+});
