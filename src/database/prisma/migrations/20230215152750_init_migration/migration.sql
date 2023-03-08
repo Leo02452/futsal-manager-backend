2e517feb0544ab28de711ef0f39a41e91e2b5972
Leo Araujo <leonardo_02452@hotmail.com>
2023-03-08 12:05:49 -0300

    chore: change player foreign key on match_events table

---
+++ src/database/prisma/migrations/20230215152750_init_migration/migration.sql
@@ -0,0 +1,126 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_id_key`(`id`),
+    UNIQUE INDEX `users_email_key`(`email`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `teams` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(25) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `teams_id_key`(`id`),
+    UNIQUE INDEX `teams_user_id_key`(`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `players` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `team_id` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `players_id_key`(`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `matches` (
+    `id` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `local` VARCHAR(191) NOT NULL,
+    `team_id` VARCHAR(191) NOT NULL,
+    `opponent` VARCHAR(191) NOT NULL,
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `matches_id_key`(`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `match_players` (
+    `player_id` VARCHAR(191) NOT NULL,
+    `match_id` VARCHAR(191) NOT NULL,
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`player_id`, `match_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `events` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `events_id_key`(`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `match_events` (
+    `id` VARCHAR(191) NOT NULL,
+    `event_id` VARCHAR(191) NOT NULL,
+    `event_player_id` VARCHAR(191) NOT NULL,
+    `match_id` VARCHAR(191) NOT NULL,
+    `has_assist` BOOLEAN NOT NULL,
+    `assist_player_id` VARCHAR(191) NOT NULL,
+    `game` VARCHAR(25) NOT NULL,
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `match_events_id_key`(`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `match_results` (
+    `match_id` VARCHAR(191) NOT NULL,
+    `second_team_score` INTEGER NOT NULL,
+    `second_opp_score` INTEGER NOT NULL,
+    `second_result` INTEGER NOT NULL,
+    `second_team_foul` INTEGER NOT NULL,
+    `second_opp_foul` INTEGER NOT NULL,
+    `first_team_score` INTEGER NOT NULL,
+    `first_opp_score` INTEGER NOT NULL,
+    `first_result` INTEGER NOT NULL,
+    `first_team_foul` INTEGER NOT NULL,
+    `first_opp_foul` INTEGER NOT NULL,
+    `update_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `match_results_match_id_key`(`match_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `teams` ADD CONSTRAINT `teams_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `players` ADD CONSTRAINT `players_team_id_fkey` FOREIGN KEY (`team_id`) REFERENCES `teams`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `matches` ADD CONSTRAINT `matches_team_id_fkey` FOREIGN KEY (`team_id`) REFERENCES `teams`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `match_players` ADD CONSTRAINT `match_players_player_id_fkey` FOREIGN KEY (`player_id`) REFERENCES `players`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `match_players` ADD CONSTRAINT `match_players_match_id_fkey` FOREIGN KEY (`match_id`) REFERENCES `matches`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `match_events` ADD CONSTRAINT `match_events_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `match_events` ADD CONSTRAINT `match_events_event_player_id_fkey` FOREIGN KEY (`event_player_id`) REFERENCES `players`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `match_events` ADD CONSTRAINT `match_events_match_id_fkey` FOREIGN KEY (`match_id`) REFERENCES `matches`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `match_events` ADD CONSTRAINT `match_events_assist_player_id_fkey` FOREIGN KEY (`assist_player_id`) REFERENCES `players`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `match_results` ADD CONSTRAINT `match_results_match_id_fkey` FOREIGN KEY (`match_id`) REFERENCES `matches`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
