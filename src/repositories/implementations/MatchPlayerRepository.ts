962de0b79fdef9f8ab2b5ee2dc0e02a81ebf3410
Leo Araujo <leonardo_02452@hotmail.com>
2023-03-08 15:05:19 -0300

    feat: add list matchPlayer repository interface and implementation

--- src/repositories/implementations/MatchPlayerRepository.ts
+++ src/repositories/implementations/MatchPlayerRepository.ts
@@ -1,8 +1,10 @@
 import prismaModel from '../../database/prisma';
 import { IMatchPlayer } from '../../entities/IMatch';
-import { ICreateMatchPlayerRepository } from '../IMatchPlayerRepository';
+import { ICreateMatchPlayerRepository, IFindMatchPlayerByMatch } from '../IMatchPlayerRepository';
 
-export default class MatchPlayerRepository implements ICreateMatchPlayerRepository {
+export default class MatchPlayerRepository implements
+ICreateMatchPlayerRepository,
+IFindMatchPlayerByMatch {
   constructor(
     private _model: typeof prismaModel.matchPlayer,
   ) { }
@@ -11,5 +13,19 @@ export default class MatchPlayerRepository implements ICreateMatchPlayerReposito
     await this._model.createMany({
       data: matchPlayersList,
     });
+  }
+
+  async getAll(matchId: string): Promise<IMatchPlayer[]> {
+    const matchPlayersList = await this._model.findMany({
+      where: { matchId },
+      include: {
+        player: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    return matchPlayersList;
   }
 }
