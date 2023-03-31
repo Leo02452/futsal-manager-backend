dffede6e6f43226126dc0c898d41975045bc89fc
Leo Araujo <leonardo_02452@hotmail.com>
2023-03-31 18:45:04 -0300

    feat: add list by matchId in matchEvent repository

--- src/repositories/implementations/MatchEventRepository.ts
+++ src/repositories/implementations/MatchEventRepository.ts
@@ -1,14 +1,34 @@
 import prismaModel from '../../database/prisma';
-import { IMatchEventToSave } from '../../entities/IMatchEvent';
-import { ICreateMatchEventRepository } from '../IMatchEventRepository';
+import { ICreatedMatchEvent, IMatchEventToSave } from '../../entities/IMatchEvent';
+import { ICreateMatchEventRepository, IFindMatchEventsRepository } from '../IMatchEventRepository';
 
 export default class MatchEventRepository implements
-ICreateMatchEventRepository {
+ICreateMatchEventRepository,
+IFindMatchEventsRepository {
   constructor(
     private _model: typeof prismaModel.matchEvent,
   ) { }
 
   async save(data: IMatchEventToSave) {
     await this._model.create({ data });
+  }
+
+  async getAll(matchId: string, game: string): Promise<ICreatedMatchEvent[]> {
+    const matchEventsList = await this._model.findMany({
+      where: { matchId, game },
+      include: {
+        eventPlayer: {
+          select: { name: true },
+        },
+        assistPlayer: {
+          select: { name: true },
+        },
+        event: {
+          select: { name: true },
+        },
+      },
+    });
+
+    return matchEventsList;
   }
 }
